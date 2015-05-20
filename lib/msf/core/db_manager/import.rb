70b60efecb91d720cc23aa68f4c8dcf2517a433d
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-05-20 09:08:34 -0500

    Revert "MSP-9532: Increase REXML expansion text limit"

--- lib/msf/core/db_manager/import.rb
+++ lib/msf/core/db_manager/import.rb
@@ -129,8 +129,6 @@ module Msf::DBManager::Import
       end
     end
 
-    # Override REXML's expansion text limit to 50k (default: 10240 bytes)
-    REXML::Security.entity_expansion_text_limit = 51200
 
     if block
       import(args.merge(:data => data)) { |type,data| yield type,data }
